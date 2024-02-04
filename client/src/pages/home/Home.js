242e27bff8c071e9a38519b2fb9a4c16076d1622
Parthapratim Deuri <parthapratimdeuri4@gmail.com>
2024-02-04 12:20:42 +0530

    add bars

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import NavBar from '../../components/NavBar';
 const Home = () => {
   return (
     <div>
+        <NavBar/>
         <AccountCircleIcon/> 
     </div>
   )
