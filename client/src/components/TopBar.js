f80f310e7a1c73b566e4bf8b2b74079e7710b5c6
Parthapratim Deuri <parthapratimdeuri4@gmail.com>
2024-02-05 09:52:20 +0530

    topbar finish

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -15,17 +15,45 @@ const TopBar = () => {
       </div>
       {/* center */}
       <div className="w-1/2">
-        <label className="bg-white rounded p-1 flex">
+        <label className="bg-white rounded-3xl p-1 flex">
           <SearchIcon className='w-1/12 mx-1' />
           <input type='text' placeholder='Search Here' className='rounded-sm pl-2 focus:outline-none w-11/12' />
         </label>
       </div>
       {/* right */}
       <div className="flex gap-4 mr-4  h-8 items-center w-1/4 justify-end">
-        <PersonIcon className='hover:text-white cursor-pointer' />
-        <NotificationsIcon className='hover:text-white cursor-pointer' />
-        <MessageIcon className='hover:text-white cursor-pointer' />
-        <SettingsIcon className='hover:text-white cursor-pointer' />
+        <div className="flex items-center">
+          <img
+            className='rounded-full h-8 w-8 object-cover border-2 border-transparent hover:border-white cursor-pointer'
+            src={"https://avatars.githubusercontent.com/u/130203363?v=4"}
+            alt="DP"
+          />
+        </div>
+        <div className="relative">
+          <PersonIcon className='hover:text-white cursor-pointer' />
+          <span
+            className='absolute top-[-4px] right-[-2px] z-[-1] text-[12px] bg-red-500 rounded-full h-4 w-4 flex items-center justify-center'>
+            1
+          </span>
+        </div>
+        <div className="relative">
+          <MessageIcon className='hover:text-white cursor-pointer' />
+          <span
+            className='absolute top-[-4px] right-[-2px]  text-[12px] bg-red-500 rounded-full h-4 w-4 flex items-center justify-center'>
+            1
+          </span>
+        </div>
+        <div className="relative">
+          <NotificationsIcon className='hover:text-white cursor-pointer' />
+          <span
+            className='absolute top-[-4px] right-[-2px] z-[-1] text-[12px] bg-red-500 rounded-full h-4 w-4 flex items-center justify-center'>
+            1
+          </span>
+        </div>
+        <div className="relative">
+          <SettingsIcon className='hover:text-white cursor-pointer' />
+
+        </div>
       </div>
 
     </div>
